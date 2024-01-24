3fb0e67786773ed77bbbe7f2255abd533aaaf31f
fredrikkri <fjkri@hotmail.com>
2024-01-24 09:22:50 +0100

    la til riktig reference til airport liberary i program.cs

--- brusOgPotetgull.gruppeoppgave/Program.cs
+++ brusOgPotetgull.gruppeoppgave/Program.cs
@@ -1,5 +1,15 @@
-﻿Fly newflight = new Fly("SAS", "Jet 34", 200);
-newflight.printFlyInformation();
+﻿using brusOgPotetgull.gruppeoppgave.core;
+namespace brusOgPotetgull.gruppeoppgave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fly newflight = new Fly("SAS", "Jet 34", 200);
+            newflight.printFlyInformation();
 
-Airport newAirport = new Airport("Militærflyplass", "Rygge Flyplass", "Rygge");
-newAirport.printAirportInformation();
+            Airport newAirport = new Airport("Militærflyplass", "Rygge Flyplass", "Rygge");
+            newAirport.printAirportInformation();
+        }
+    }
+}
--- brusOgPotetgull.gruppeoppgave/obj/Debug/net8.0/brusOgPotetgull.gruppeoppgave.AssemblyInfo.cs
+++ brusOgPotetgull.gruppeoppgave/obj/Debug/net8.0/brusOgPotetgull.gruppeoppgave.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("brusOgPotetgull.gruppeoppgave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7fdf30df82a382240e54de86636cc699d217cd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("brusOgPotetgull.gruppeoppgave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("brusOgPotetgull.gruppeoppgave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
