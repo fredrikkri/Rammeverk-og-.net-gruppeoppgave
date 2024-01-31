e2675c15875b355557c41c7eb330f5a86dbce60a
JacobH0st <jacobhost02@gmail.com>
2024-01-31 15:10:54 +0100

    oppdatering på flyvinger
    når man starter å simulere en flyving vil den ikke lenger gjenta seg hele minuttet.
    man kan også nå sette opp flygninger som gjentar seg og se når de gjentar seg

--- brusOgPotetgull.airportLiberary/Flight.cs
+++ brusOgPotetgull.airportLiberary/Flight.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Security;
 using System.Reflection;
 
 namespace brusOgPotetgull.airportLiberary
@@ -87,35 +88,34 @@ namespace brusOgPotetgull.airportLiberary
             // checks if the plane are adjusted for gates.
             if (DepartureGate.CheckIfAircraftCanUseGate(ActiveAicraft) && ArrivalGate.CheckIfAircraftCanUseGate(ActiveAicraft) == true)
             {
-                while (flightDate < DateTime.Now)
+                
+                // If the date it right, the flight will proceed. We dont care about seconds.
+                if (flightDate.Year == DateTime.Now.Year &&
+                    flightDate.Month == DateTime.Now.Month &&
+                    flightDate.Hour == DateTime.Now.Hour &&
+                    flightDate.Minute == DateTime.Now.Minute)
                 {
-                    // If the date it right, the flight will proceed. We dont care about seconds.
-                    if (flightDate.Year == DateTime.Now.Year &&
-                        flightDate.Month == DateTime.Now.Month &&
-                        flightDate.Hour == DateTime.Now.Hour &&
-                        flightDate.Minute == DateTime.Now.Minute)
-                    {
-                        // Start-gate
-                        ActiveAicraft.AddHistoryToAircraft("Gate " + DepartureGate.GetIdAndAirportNickname(), ", Left Gate");
-                        Console.Write($"\n{ActiveAicraft.Model} has Left Gate\n");
+                    // Start-gate
+                    ActiveAicraft.AddHistoryToAircraft("Gate " + DepartureGate.GetIdAndAirportNickname(), ", Left Gate");
+                    Console.Write($"\n{ActiveAicraft.Model} has Left Gate\n");
 
-                        // Taxiway
-                        DepartureTaxiway.AddAircraftToQueue(ActiveAicraft);
-                        DepartureTaxiway.PeekToSeIfYourAircraftIsNext(ActiveAicraft);
+                    // Taxiway
+                    DepartureTaxiway.AddAircraftToQueue(ActiveAicraft);
+                    DepartureTaxiway.PeekToSeIfYourAircraftIsNext(ActiveAicraft);
 
-                        // Runway
-                        var speedAfterTakeoff = DepartureRunway.SimulateTakeoff(ActiveAicraft);
+                    // Runway
+                    var speedAfterTakeoff = DepartureRunway.SimulateTakeoff(ActiveAicraft);
 
-                        // In air
-                        SimulateAirTime(speedAfterTakeoff);
+                    // In air
+                    SimulateAirTime(speedAfterTakeoff);
 
-                        // Arrival-gate
-                        ActiveAicraft.AddHistoryToAircraft("Gate " + ArrivalGate.GetIdAndAirportNickname(), ", Arrived at Gate");
-                    }
-                    // checks every minute to see if the date of the flight is now.
-                    Thread.Sleep(1000);
+                    // Arrival-gate
+                    ActiveAicraft.AddHistoryToAircraft("Gate " + ArrivalGate.GetIdAndAirportNickname(), ", Arrived at Gate");
                 }
+                // checks every minute to see if the date of the flight is now.
+                Thread.Sleep(1000);
             }
+            
             else
             {
                 Console.Write($"\nFlight with id '{flightId}': One of the gates does not fit with the plane. The flight cannot be done...\n");
@@ -125,8 +125,10 @@ namespace brusOgPotetgull.airportLiberary
         {
             for (int i = 0; i < numberOfDays; i++)
             {
-                Console.Write($"\ndate of flight: {dateFlight}\n");
+                
                 StartFlight(dateFlight.AddDays(i));
+                Console.Write($"\ndate of flight: \n" + dateFlight.AddDays(i));
+                
             }
         }
     }
