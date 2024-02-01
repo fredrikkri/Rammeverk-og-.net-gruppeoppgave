4e823da28ed9024fc4d3ab1a4571e94c284ef570
fredrikkri <fjkri@hotmail.com>
2024-02-01 18:49:13 +0100

    endret navn på funksjon

--- brusOgPotetgull.gruppeoppgave/Program.cs
+++ brusOgPotetgull.gruppeoppgave/Program.cs
@@ -7,16 +7,15 @@ namespace brusOgPotetgull.gruppeoppgave
     {
         static void Main(string[] args)
         {
-            
             Aircraft cargoCraftV12 = new CargoAircraft("Cargo plane v12", 890, 50, 40, 5); cargoCraftV12.PrintAircraftInformation();
-            Aircraft superPlane = new CargoAircraft("Cristiano Ronaldo´s Plane, siuuuuu!", 800, 56, 20, 3);
+            Aircraft superPlane = new LightAircraft("Cristiano Ronaldo´s Plane, siuuuuu!", 800, 56, 20, 3);
             Aircraft sickPlane = new CargoAircraft("Franceco Totti´s Plane.", 800, 56, 20, 3);
 
             Airport ryggeFlyplass = new Airport("RYG", "Rygge Flyplass", "Rygge");
             Airport GardemoenFlyplass = new Airport("GAR", "Gardemoen Flyplass", "Oslo");
 
-            Gate supergate = new Gate("Gate 1A", GardemoenFlyplass); supergate.AddLegalAircraftThatCanUseGate(cargoCraftV12.AircraftTypeId);
-            Gate nissegate = new Gate("Gate 22", ryggeFlyplass); nissegate.AddLegalAircraftThatCanUseGate(cargoCraftV12.AircraftTypeId);
+            Gate supergate = new Gate("Gate 1A", GardemoenFlyplass); supergate.AddLegalAircraftThatCanUseGate(cargoCraftV12.AircraftTypeId); supergate.AddLegalAircraftThatCanUseGate(superPlane.AircraftTypeId);
+            Gate nissegate = new Gate("Gate 22", ryggeFlyplass); nissegate.AddLegalAircraftThatCanUseGate(cargoCraftV12.AircraftTypeId); nissegate.AddLegalAircraftThatCanUseGate(superPlane.AircraftTypeId);
 
             Taxiway mediumTaxiway = new Taxiway(535, 20, GardemoenFlyplass);
             Taxiway longTaxiway = new Taxiway(75, 35, ryggeFlyplass);
@@ -28,7 +27,7 @@ namespace brusOgPotetgull.gruppeoppgave
             Flight coolFlight = new Flight(superPlane, 5000, GardemoenFlyplass, ryggeFlyplass, supergate, nissegate, mediumTaxiway, longTaxiway, gammelRunway, slitenRunway);
             Flight coolFlight2 = new Flight(sickPlane, 5000, GardemoenFlyplass, ryggeFlyplass, supergate, nissegate, mediumTaxiway, longTaxiway, gammelRunway, slitenRunway);
 
-            coolFlight.StartFlight(DateTime.Now);
+            coolFlight.SetupFlight(DateTime.Now);
 
 
             //coolFlight.SetupDailyFlight(DateTime.Now.AddDays(0), 3);
--- brusOgPotetgull.gruppeoppgave/obj/Debug/net8.0/brusOgPotetgull.gruppeoppgave.AssemblyInfo.cs
+++ brusOgPotetgull.gruppeoppgave/obj/Debug/net8.0/brusOgPotetgull.gruppeoppgave.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("brusOgPotetgull.gruppeoppgave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+068cdf7e43bc5439576cf64f913ae548249f20b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d58da4181e49c07eabc0d9d990f982a964e0176d")]
 [assembly: System.Reflection.AssemblyProductAttribute("brusOgPotetgull.gruppeoppgave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("brusOgPotetgull.gruppeoppgave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
