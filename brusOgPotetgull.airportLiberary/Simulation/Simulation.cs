b38668cf520a150b454d90022e34e7c4880320aa
Andrea <andrea.s.nilsson@hiof.no>
2024-02-02 17:15:31 +0100

    lagt til kommentarer i simulation for å sjekke at vi har tenkt riktig og lettere se hva vi mangler av logikk

--- brusOgPotetgull.airportLiberary/Simulation/Simulation.cs
+++ brusOgPotetgull.airportLiberary/Simulation/Simulation.cs
@@ -3,37 +3,43 @@ namespace brusOgPotetgull.airportLiberary.Simulation
 {
 	public class Simulation
 	{
+
+        // Starter simuleringen 
         public Simulation(Airport airport, int year, int month, int day, int hour, int min)
         {
+            // Oppretter et objekt for å angi sluttid for simuleringens varighet
             DateTime datetimeEnd = new DateTime(year, month, day, hour, min, 0);
+
+            // Simulering starter, og kjører inntil tidspunktet for endelse er større eller lik starttidspunktet
             while (DateTime.Now <= datetimeEnd)
             {
+                // For hver innkommende Flight i luften som ønsker å lande på rullebane...
                 foreach (var incommingAircraft in airport.GetInncommingAircrafts())
                 {
+                    // Flight som ønsker å lande, legges inn i en "Flight in air queue for landing"
                     airport.AddToInncommingAircrafts(incommingAircraft);
+
+
+                    // TODO: Fjerne flyene fra køen "GetIncomingAircraft"
                 }
 
+                // Sjekke om det eksisterer Flight som befinner seg på Taxiway, i kø til Runway for Departure
                 foreach (var departuringAircraft in airport.GetDeparturingAircrafts())
                 {
+                    // Flights i listen departuringAircraft[] legges til i køen AddToDeaprturingQueue() 
                     airport.AddToDeparturingQueue(departuringAircraft);
                 }
-
-                    
-                    
-                    
-
-                ///finne fly som skal lette(for loop ?)
-                ///legge dem til i kø(kom til taxebane)
-
+                
+                // Sjekker om det er fly i lufta som MÅ lande og dermed okkupere rullebane
                 if (airport.GetInncommingAircrafts().Count != 0)
                 {
-                ///land fly 1 i kø(husk å fjærne fly)
+                ///land fly 1 i kø(husk å fjerne fly)
                 ///og gå til taxebane exit
                 ///leg til kø til gate er ledig
                 }
                 else
                 {
-                ///lett fly 1 i kø(husk å fjærne fly)
+                ///lett fly 1 i kø(husk å fjerne fly) fra DeparturingQueue
                 }
             }
         }
