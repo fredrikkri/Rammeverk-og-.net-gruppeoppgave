2fd721939b9fef1037796a4cc1c135d94c62ce74
fredrikkri <fjkri@hotmail.com>
2024-02-05 21:42:46 +0100

    måtte ha med disse ubrukelige filene for at det skulle fungere

--- brusOgPotetgull.airportLiberary/obj/Debug/net8.0/brusOgPotetgull.airportLiberary.AssemblyInfo.cs
+++ brusOgPotetgull.airportLiberary/obj/Debug/net8.0/brusOgPotetgull.airportLiberary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("brusOgPotetgull.airportLiberary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df46cf1b63d43ff6edd7fb1c72b770d7796fb082")]
 [assembly: System.Reflection.AssemblyProductAttribute("brusOgPotetgull.airportLiberary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("brusOgPotetgull.airportLiberary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
