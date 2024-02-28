1925291afb4f3648b309992faf5c921e756bafae
fredrikkri <fjkri@hotmail.com>
2024-02-28 09:39:55 +0100

    laget enum for å velge hvilken aircrafttype

---
+++ brusOgPotetgull.airportLiberary/AircraftTypes/ChooseAircraftType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace brusOgPotetgull.airportLiberary.AircraftTypes
+{
+	public enum ChooseAircraftType
+	{
+		Aircraft = 0,
+		LightAircraft = 1,
+        ShortMediumAircraft = 2,
+        LongMediumAircraft = 3,
+        LargeAircraft = 4,
+        CargoAircraft = 5,
+		MilitaryAircraft = 6
+	}
+}
+
