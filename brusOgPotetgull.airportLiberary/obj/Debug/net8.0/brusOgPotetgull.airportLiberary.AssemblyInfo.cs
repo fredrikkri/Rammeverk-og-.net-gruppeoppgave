d3fd2a03eb90899879da6589fc283367b1580a56
evjengit <ole_fram.96@hotmail.com>
2024-02-29 14:23:42 +0100

    damn assembly

--- brusOgPotetgull.airportLiberary/obj/Debug/net8.0/brusOgPotetgull.airportLiberary.AssemblyInfo.cs
+++ brusOgPotetgull.airportLiberary/obj/Debug/net8.0/brusOgPotetgull.airportLiberary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("brusOgPotetgull.airportLiberary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8913e415f0be3e794bfd6b5b084baaf5c7200e53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e87c151eda0c1adcdb4db891b349a6b69529d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("brusOgPotetgull.airportLiberary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("brusOgPotetgull.airportLiberary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
