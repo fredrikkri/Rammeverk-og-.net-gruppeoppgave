3904d8998ebd00488cdd73091eb85db7146dc574
fredrikkri <fjkri@hotmail.com>
2024-03-01 13:54:07 +0100

    la til ny custom exception og la til litt exception logikk

---
+++ brusOgPotetgull.airportLiberary/CustomExceptions/NegativeNumberException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace brusOgPotetgull.airportLiberary.CustomExceptions
+{
+    public class NegativeNumberException : Exception
+    {
+        public NegativeNumberException()
+            : base()
+        {
+
+        }
+        public NegativeNumberException(string message)
+            : base(message)
+        {
+
+        }
+        public NegativeNumberException(string message, Exception inner)
+            : base(message, inner)
+        {
+
+        }
+    }
+}
