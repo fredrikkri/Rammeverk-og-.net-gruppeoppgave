04db466e68810b27f5f7759ac22a30f76d9a5f51
evjengit <ole_fram.96@hotmail.com>
2024-04-08 16:01:08 +0200

    videreutvikling av GUI

--- brusOgPotetgull.userInterface/brusOgPotetgull.userInterface/MainPage.xaml.cs
+++ brusOgPotetgull.userInterface/brusOgPotetgull.userInterface/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace brusOgPotetgull.userInterface
+﻿using BrusOgPotetgull.AirportLiberary;
+namespace brusOgPotetgull.userInterface
 {
     public partial class MainPage : ContentPage
     {
@@ -12,7 +13,6 @@
         private void OnCounterClicked(object sender, EventArgs e)
         {
             count++;
-
             if (count == 1)
                 CounterBtn.Text = $"Clicked {count} time";
             else
@@ -20,6 +20,13 @@
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
+        
+        private void OnAirportClicked(object sender, EventArgs e)
+        {
+            var airportCode = Paramentry1.Text;
+            var name = Paramentry2.Text;
+            var location = Paramentry3.Text;
+            _ = new Airport(airportCode, name, location);
+        }
     }
-
 }
