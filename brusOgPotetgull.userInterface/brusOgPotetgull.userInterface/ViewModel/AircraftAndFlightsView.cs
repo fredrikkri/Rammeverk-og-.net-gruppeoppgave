29ad1fdb1926bd172f1e4e3eb7a8cbfe8737d516
fredrikkri <fjkri@hotmail.com>
2024-04-12 12:51:01 +0200

    lagt til noen nye tabs i gui

---
+++ brusOgPotetgull.userInterface/brusOgPotetgull.userInterface/ViewModel/AircraftAndFlightsView.cs
@@ -0,0 +1,15 @@
+﻿namespace brusOgPotetgull.userInterface.ViewModel;
+
+public class AircraftAndFlightsView : ContentPage
+{
+	public AircraftAndFlightsView()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
